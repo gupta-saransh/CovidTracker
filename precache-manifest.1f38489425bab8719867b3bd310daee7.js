3cdf279652ac228835548c0cf7a989032302260e
gupta-saransh <saransh287@gmail.com>
2020-08-06 23:28:01 +0530

    Updates

---
+++ precache-manifest.1f38489425bab8719867b3bd310daee7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a291cf2d9f68022707b33f8533bc6713",
+    "url": "/CovidTracker.git/index.html"
+  },
+  {
+    "revision": "5b7dc5c0e500e5382599",
+    "url": "/CovidTracker.git/static/css/main.f6747676.chunk.css"
+  },
+  {
+    "revision": "caea90334dfe03b277e9",
+    "url": "/CovidTracker.git/static/js/2.6b019359.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CovidTracker.git/static/js/2.6b019359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7dc5c0e500e5382599",
+    "url": "/CovidTracker.git/static/js/main.db679bab.chunk.js"
+  },
+  {
+    "revision": "70806b2889c7216b2ad8",
+    "url": "/CovidTracker.git/static/js/runtime-main.944d1471.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CovidTracker.git/static/media/image.d7265326.png"
+  }
+]);
